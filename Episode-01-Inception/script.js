ab39bb89f7f3aa742164877d2f7a7cd97f4bcc06
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-06 07:56:34 +0530

    Putting two or more child under one child elements

--- Episode-01-Inception/script.js
+++ Episode-01-Inception/script.js
@@ -1,22 +1,34 @@
 
 
-// First react code 
+// First react code *************************************************************** //
 
 // const heading = React.createElement("h1", {id: "heading"}, "hello from react code");
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 
 
-// putting elements under elements 
+// putting child elements under child elements *************************************************************** //
 
-const parent = React.createElement("div", { id: "parent" }, 
-    React.createElement("div", { id: "child" }, 
-        React.createElement("h1", { id: "heading" }, "This is the main heading tag" )
-    )
-);
+// const parent = React.createElement("div", { id: "parent" }, 
+//     React.createElement("div", { id: "child" }, 
+//         React.createElement("h1", { id: "heading" }, "This is the main heading tag" )
+//     )
+// );
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(parent);
-root.render(parent);
+// console.log(parent);
+// root.render(parent);
+
+
+// Putting two or more child under one child elements *************************************************************** //
+
+// const heading = React.createElement("div", { id: "parent" }, 
+//     React.createElement( "div", { id: "child" }, [
+//         React.createElement( "h1", { id: "heading1" }, "This is the first Heading" ),
+//         React.createElement( "h2", { id: "heading2" }, "This is the second Heading" )
+//         ]
+//     )
+// )
+// root.render(heading);
 
 
 // 
