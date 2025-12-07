d4d58feb875a1454e2eabc072b2fb2b10363db11
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-07 06:54:43 +0530

    json file

--- ep-02-ignitingOurApp/script.js
+++ ep-02-ignitingOurApp/script.js
@@ -1,16 +1,13 @@
-
-
 // First react code *************************************************************** //
 
 // const heading = React.createElement("h1", {id: "heading"}, "hello from react code");
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 
-
 // putting child elements under child elements *************************************************************** //
 
-// const parent = React.createElement("div", { id: "parent" }, 
-//     React.createElement("div", { id: "child" }, 
+// const parent = React.createElement("div", { id: "parent" },
+//     React.createElement("div", { id: "child" },
 //         React.createElement("h1", { id: "heading" }, "This is the main heading tag" )
 //     )
 // );
@@ -18,10 +15,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 // console.log(parent);
 // root.render(parent);
 
-
 // Putting two or more child under one child elements ************************************************************** //
 
-// const heading = React.createElement("div", { id: "parent" }, 
+// const heading = React.createElement("div", { id: "parent" },
 //     React.createElement( "div", { id: "child" }, [
 //         React.createElement( "h1", { id: "heading1" }, "This is the first Heading" ),
 //         React.createElement( "h2", { id: "heading2" }, "This is the second Heading" )
@@ -30,5 +26,4 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 // )
 // root.render(heading);
 
-
-// 
+//
